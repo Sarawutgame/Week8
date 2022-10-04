50e9d0ae3f4639f09a6cb680481294614930d408
iioUHoD <iiouhod@gmail.com>
2022-10-04 15:58:10 +0700

    Initial commit

---
+++ src/main/java/com/example/week8/Week8Application.java
@@ -0,0 +1,17 @@
+package com.example.week8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Week8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week8Application.class, args);
+    }
+
+}
+
+
